31b370f22212e6fb187c9c9589db973949c1691a
Hazem Essam <133036561+Hazem1199@users.noreply.github.com>
2023-12-11 20:00:26 +0200

    11/12/2023

--- js pages/group.js
+++ js pages/group.js
@@ -5,9 +5,12 @@ const frmDiv2 = document.querySelector('.frmDiv2');
 const btnNext = document.querySelector('#btnNext');
 const containerForm = document.querySelector('.containerForm');
 const StudentNum = document.querySelector('#StudentNum');
+const ActuallyDate = document.querySelector(".ActuallyDate")
+const DateBySRM = document.querySelector(".DateBySRM")
+const by = document.querySelector(".by")
 
 
-containerForm
+
 
 var overlay = document.createElement("div");
 overlay.style.position = "fixed";
@@ -46,6 +49,16 @@ function hide() {
 // }
 
 
+async function getOnlineSending(id) {
+  const response = await fetch('https://script.google.com/macros/s/AKfycbxRtzdcG3n8fdTqLmg8qPPEWniywl4jrlhru0qadBtDUQx8lsUtgJqpOOVm_HS6ybyRKg/exec', {
+    method: 'POST',
+    body: JSON.stringify({ "id": id })
+  })
+  var data = await response.json();
+  console.log(data);
+  return data;
+}
+
 async function getInfoGroup(id) {
   const response = await fetch('https://script.google.com/macros/s/AKfycbwAIsY5c7ebcjrDN58-l097iqHF-_Rd6gChIbA4rcKggLD0qgVP95fgm2oHS8FjhxM5Iw/exec', {
     method: 'POST',
@@ -58,17 +71,43 @@ async function getInfoGroup(id) {
 // Initialize the counter
 let k = 0;
 
+let onlinePush = []
+
 
 async function showAllGroup(id) {
   try {
     change()
     const groups = await getInfoGroup(id);
+    const onlineSend = await getOnlineSending(id);
     let tableBody = document.querySelector('.divTableBody');
     tableBody.innerHTML = '';
     //let i = 0;
     // console.log("group.ID" +group.ID);
     console.log("id" + id);
+    // get online sending
 
+    for (let i = 0; i < onlineSend.length; i++) {
+      if (id == onlineSend[i].ID) {
+        var onlineSendObj = {
+          ID: onlineSend[i].ID,
+          ActuallySendingDate: onlineSend[i]["Actually Sending Date"],
+          ModuleCode: onlineSend[i]["Module Code"],
+          ActuallySendingDateBySRM: onlineSend[i]["Actually Sending Date by SRM"],
+          Code: onlineSend[i].Code,
+          User: onlineSend[i].User,
+        };
+      }
+      onlinePush.push(onlineSendObj)
+      //   console.log(onlinePush);
+    }
+
+
+
+
+
+
+
+    // get all data of modules
     groups.filter(group => group.ID == id).forEach(async group => {
       // if (group.ID === id) {
       console.log(group);
@@ -101,7 +140,7 @@ async function showAllGroup(id) {
         grade5: group[`g5 grade`],
         attendance5: group[`g5 attendance`],
         Group6: group[`g6`],
-        Module6: group[`g1 module`],
+        Module6: group[`g6 module`],
         Date6: group[`g6 date`],
         grade6: group[`g6 grade`],
         attendance6: group[`g6 attendance`],
@@ -139,17 +178,15 @@ async function showAllGroup(id) {
       };
 
       for (var i = 1; i <= 12; i++) {
-        let row = document.createElement('tr')
+        // Create a new row
+        let row = document.createElement('tr');
         row.classList.add('divTableRow');
 
-        // Set the date to i days after the original date
+        // Set the date and formatted date
         let dateProp = `Date${i}`;
         const date = new Date(student[dateProp]);
-        // date.setDate(date.getDate() - 1);
         const options = { year: 'numeric', month: 'numeric', day: 'numeric' };
         let formattedDate = date.toLocaleDateString('en-GB', options);
-        // let newDate = new Date(student[dateProp]);
-        // newDate.setDate(newDate.getDate() + i);
 
         // Get the properties for the current group
         let groupProp = `Group${i}`;
@@ -160,22 +197,66 @@ async function showAllGroup(id) {
         let module = student[moduleProp];
         let grade = student[gradeProp];
         let attendance = student[attendanceProp];
+        if (onlinePush.length > 0) {
+          var ActuallySendingDate = onlinePush.ActuallySendingDate;
+          var ActuallySendingDateBySRM = onlinePush.ActuallySendingDateBySRM;
+          var User = onlinePush.User;
+          var ModuleCode = onlinePush.ModuleCode;
+        }
 
 
-        // Save the group and formattedDate to session storage
-        sessionStorage.setItem('group', group);
-        sessionStorage.setItem('formattedDate', formattedDate);
-        console.log(group);
-        console.log(formattedDate);
+              // Save the group and formattedDate to session storage
+              sessionStorage.setItem('group', group);
+              sessionStorage.setItem('formattedDate', formattedDate);
+              console.log(group);
+              console.log(formattedDate);
 
 
+        // Filter online push data based on module
+
+        // Check if data exists for this module
+        // Filter data and check if it exists
+        const filteredData = onlinePush.filter((x) => x.ModuleCode === group);
+        if (filteredData.length > 0) {
+          // Do something with the filtered data
+          var ActuallySendingDate = new Date(filteredData[0].ActuallySendingDate);
+          var ActuallySendingDateBySRM = new Date(filteredData[0].ActuallySendingDateBySRM);
+
+          // Format the dates
+          var formattedActuallySendingDate = ActuallySendingDate.toLocaleDateString('en-GB', options);
+          var formattedActuallySendingDateBySRM = isValidDate(ActuallySendingDateBySRM) ? ActuallySendingDateBySRM.toLocaleDateString('en-GB', options) : "";
+          
+          var User = filteredData[0].User;
+          console.log(formattedActuallySendingDate);
+        } else {
+          // Optionally, set default values if no data exists
+          var ActuallySendingDate = ""; // or any desired default value
+          var ActuallySendingDateBySRM = "";
+          var formattedActuallySendingDate = ""; // or any desired default value
+          var formattedActuallySendingDateBySRM = "";
+          var User = "";
+        }
+        //  Function to check if a date is valid
+        function isValidDate(date) {
+          return date instanceof Date && !isNaN(date);
+        }
+
+
+
+        // Add content to the row
         row.innerHTML = `
-                  <td class = "divTableCell">${formattedDate}</td>
-                  <td class = "divTableCell">${group}</td>
-                  <td class = "divTableCell">${module}</td>
-                  <td class = "divTableCell">${grade}</td>
-                  <td class = "divTableCell">${attendance}</td>
-                `
+        <td class = "divTableCell">${formattedDate}</td>
+        <td class = "divTableCell">${group}</td>
+        <td class = "divTableCell">${module}</td>
+        <td class = "divTableCell" style="color: green; font-weight: bold;">${formattedActuallySendingDate}</td>
+        <td class = "divTableCell">${formattedActuallySendingDateBySRM}</td>
+        <td class = "divTableCell">${User}</td>
+        <td class = "divTableCell">${grade}</td> 
+        <td class = "divTableCell">${attendance}</td>
+      `
+
+
+
         // Check if the row is empty
         let isEmpty = true;
         row.querySelectorAll('td').forEach(td => {
@@ -193,88 +274,91 @@ async function showAllGroup(id) {
         }
 
         const frmDiv1 = document.querySelector('.frmDiv1');
-      // Create a new div element to store the inputs
-      const inputDiv = document.createElement('div');
+        // Create a new div element to store the inputs
+        const inputDiv = document.createElement('div');
 
-      const groupp = sessionStorage.getItem('group')
-      const formattedDatee = sessionStorage.getItem('formattedDate')
-
-
-
-      // Iterate over the pushObj array
-      // for (const value of pushObj) {
-      // Create a new input element
-      const input = document.createElement('input');
-      const input1 = document.createElement('input');
-
-      // Set the value of the input element to the current value in the pushObj array
-      input.value = groupp;
-      input1.value = formattedDatee;
+        const groupp = sessionStorage.getItem('group')
+        const formattedDatee = sessionStorage.getItem('formattedDate')
 
 
-      // Set the style width of the input element & flex
-      inputDiv.style.display = ('flex');
-      input.style.width = '60%';
-      input1.style.width = '60%';
-      input.style.marginRight = '5px';
-      input.style.marginLeft = '15px';
-      input1.style.marginLeft = '15px';
 
-      // Set the type of the input element to 'number'
-      input.type = 'number';
-      // input1.type = 'Date';
+        // Iterate over the pushObj array
+        // for (const value of pushObj) {
+        // Create a new input element
+        const input = document.createElement('input');
+        const input1 = document.createElement('input');
 
-
-      if (grade) {
-        input.style.display = 'none';
-        input1.style.display = 'none';
-        // input1.disabled = true;
-
-      }
+        // Set the value of the input element to the current value in the pushObj array
+        input.value = groupp;
+        input1.value = formattedDatee;
 
 
-      // Append the input element to the inputDiv element
-      inputDiv.appendChild(input);
-      inputDiv.appendChild(input1);
+        // Set the style width of the input element & flex
+        inputDiv.style.display = ('flex');
+        input.style.width = '60%';
+        input1.style.width = '60%';
+        input.style.marginRight = '5px';
+        input.style.marginLeft = '15px';
+        input1.style.marginLeft = '15px';
+
+        // Set the type of the input element to 'number'
+        input.type = 'number';
+        // input1.type = 'Date';
 
 
-      // Increment the counter
-      k++;
-      // Set the name attribute of the input element
-      input.name = 'Group' + [k];
-      input1.name = 'GroupDate' + [k];
-      console.log(input.name);
+        if (grade) {
+          input.style.display = 'none';
+          input1.style.display = 'none';
+          // input1.disabled = true;
+
+        }
 
 
-      // Set the placeholder attribute of the input element
-      input.placeholder = 'Amount';
-      input1.placeholder = 'Due Date';
+        // Append the input element to the inputDiv element
+        inputDiv.appendChild(input);
+        inputDiv.appendChild(input1);
 
-      // Set the id attribute of the input element
-      input.id = 'Group';
-      StudentNum.value = id;
-          // StudentNum.disabled = false;
-      
 
-      // Set the class attribute of the input element
-      input.classList.add('form-control', 'Group', 'mb-3');
-      input1.classList.add('form-control', 'Group', 'mb-3');
+        // Increment the counter
+        k++;
+        // Set the name attribute of the input element
+        input.name = 'Group' + [k];
+        input1.name = 'GroupDate' + [k];
+        console.log(input.name);
 
-      // Set the required attribute of the input element
-      input.required = true;
-      // }  
-      // Save the value of k to local storage
-      localStorage.setItem('k', k);
 
-      // Append the inputDiv element to the document body
-      frmDiv1.appendChild(inputDiv);
+        // Set the placeholder attribute of the input element
+        input.placeholder = 'Amount';
+        input1.placeholder = 'Due Date';
+
+        // Set the id attribute of the input element
+        input.id = 'Group';
+        StudentNum.value = id;
+        // StudentNum.disabled = false;
+
+
+        // Set the class attribute of the input element
+        input.classList.add('form-control', 'Group', 'mb-3');
+        input1.classList.add('form-control', 'Group', 'mb-3');
+
+        // Set the required attribute of the input element
+        input.required = true;
+        // }  
+        // Save the value of k to local storage
+        localStorage.setItem('k', k);
+
+        // Append the inputDiv element to the document body
+        frmDiv1.appendChild(inputDiv);
 
       }
       // save group value to pass it to deadline form 
 
-      
+
 
     })
+
+
+
     hide()
   } catch (error) {
     console.log(error);
@@ -286,69 +370,69 @@ btnNext.onclick = function () {
   frmDiv2.style.left = "15px";
   frmDiv2.style.visibility = "visible";
   containerForm.style.height = "380px";
-  }
+}
 
 
 const alertMsg = document.querySelector('.alertMsg');
 
-  //supmit  frmGroupEdit
-  jQuery('#frmModuleEdit').on('submit', function (e) {
-    e.preventDefault();
-    jQuery.ajax({
-      url: 'https://script.google.com/macros/s/AKfycbwe6pqKwdyIsqXBIhLLUlZBwKcGacB2txW2hCIGQv5jbiqI_HpJesmnTTr2sKYYYsLSsg/exec',
-      type: 'post',
-      data: jQuery('#frmModuleEdit').serialize(),
-      beforeSend: function () {
-        var spinner = '<div class="text-center" style="margin-left: 5px;"><div class="spinner-border spinner-border-sm" role="status"><span class="visually-hidden">Loading...</span></div></div>';
+//supmit  frmGroupEdit
+jQuery('#frmModuleEdit').on('submit', function (e) {
+  e.preventDefault();
+  jQuery.ajax({
+    url: 'https://script.google.com/macros/s/AKfycbwe6pqKwdyIsqXBIhLLUlZBwKcGacB2txW2hCIGQv5jbiqI_HpJesmnTTr2sKYYYsLSsg/exec',
+    type: 'post',
+    data: jQuery('#frmModuleEdit').serialize(),
+    beforeSend: function () {
+      var spinner = '<div class="text-center" style="margin-left: 5px;"><div class="spinner-border spinner-border-sm" role="status"><span class="visually-hidden">Loading...</span></div></div>';
 
-        jQuery('#spinner-container').html(spinner);
-      },
-      success: function (result) {
-        jQuery('#frmModuleEdit')[0].reset();
-        // Display success message here
-        alertMsg.classList.add('alert', 'alert-success');
-        alertMsg.style.width = '25%';
-        alertMsg.style.position = 'fixed';
-        alertMsg.style.top = '0';
-        alertMsg.style.left = '0';
-        alertMsg.style.margin = '20px';
-        alertMsg.style.transition = "all 0.5s ease-in-out";
-        alertMsg.innerHTML = '<strong>Success!</strong> Payment edit successfully.';
-        alertMsg.style.display = "block";
-        alertMsg.style.opacity = "0";
-        setTimeout(function () {
-          alertMsg.style.opacity = "1";
-        }, 10);
-        setTimeout(function () {
-          alertMsg.style.display = "none";
-          location.reload();
+      jQuery('#spinner-container').html(spinner);
+    },
+    success: function (result) {
+      jQuery('#frmModuleEdit')[0].reset();
+      // Display success message here
+      alertMsg.classList.add('alert', 'alert-success');
+      alertMsg.style.width = '25%';
+      alertMsg.style.position = 'fixed';
+      alertMsg.style.top = '0';
+      alertMsg.style.left = '0';
+      alertMsg.style.margin = '20px';
+      alertMsg.style.transition = "all 0.5s ease-in-out";
+      alertMsg.innerHTML = '<strong>Success!</strong> Payment edit successfully.';
+      alertMsg.style.display = "block";
+      alertMsg.style.opacity = "0";
+      setTimeout(function () {
+        alertMsg.style.opacity = "1";
+      }, 10);
+      setTimeout(function () {
+        alertMsg.style.display = "none";
+        location.reload();
 
-        }, 5000);
-      },
-      error: function () {
-        // Display error message here
-        alertMsg.classList.add('alert', 'alert-danger');
-        alertMsg.style.width = '25%';
-        alertMsg.style.position = 'fixed';
-        alertMsg.style.top = '0';
-        alertMsg.style.left = '0';
-        alertMsg.style.transition = "all 0.5s ease-in-out";
-        alertMsg.innerHTML = '<strong>Error!</strong> An error occurred. Please try again.';
-        alertMsg.style.display = "block";
-        alertMsg.style.opacity = "0";
-        setTimeout(function () {
-          alertMsg.style.opacity = "1";
-        }, 10);
-        setTimeout(function () {
-          alertMsg.style.display = "none";
-        }, 2000);
-      },
-      complete: function () {
-        jQuery('#spinner-container').empty();
-      }
-    });
+      }, 5000);
+    },
+    error: function () {
+      // Display error message here
+      alertMsg.classList.add('alert', 'alert-danger');
+      alertMsg.style.width = '25%';
+      alertMsg.style.position = 'fixed';
+      alertMsg.style.top = '0';
+      alertMsg.style.left = '0';
+      alertMsg.style.transition = "all 0.5s ease-in-out";
+      alertMsg.innerHTML = '<strong>Error!</strong> An error occurred. Please try again.';
+      alertMsg.style.display = "block";
+      alertMsg.style.opacity = "0";
+      setTimeout(function () {
+        alertMsg.style.opacity = "1";
+      }, 10);
+      setTimeout(function () {
+        alertMsg.style.display = "none";
+      }, 2000);
+    },
+    complete: function () {
+      jQuery('#spinner-container').empty();
+    }
   });
-  
+});
+
 
 
 
